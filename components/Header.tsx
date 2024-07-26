7ba1dc42047dd0b07bab19310e4ba9973da85e18
chenjingdev <chenjingdev@gmail.com>
2024-07-26 22:23:33 +0900

    로고 텍스트 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,9 +6,7 @@ import { leftAsideVisibleState } from '@/recoil/atoms';
 import { useRecoilState } from 'recoil';
 
 export default function Header() {
-  const [leftAsideVisible, setLeftAsideVisible] = useRecoilState(
-    leftAsideVisibleState,
-  );
+  const [leftAsideVisible, setLeftAsideVisible] = useRecoilState(leftAsideVisibleState);
 
   const toggleAside = () => {
     setLeftAsideVisible(!leftAsideVisible);
@@ -18,13 +16,7 @@ export default function Header() {
     <div className="group h-[60px] w-full border-b flex justify-between items-center p-5">
       <div className="flex justify-between w-[292px]">
         <div>
-          <svg
-            viewBox="0 0 100 100"
-            width="45"
-            height="45"
-            stroke="black"
-            strokeWidth="3"
-          >
+          <svg viewBox="0 0 100 100" width="45" height="45" stroke="black" strokeWidth="3">
             <circle cx="32" cy="20" r="6" fill="black" />
             <line x1="32" y1="20" x2="58" y2="50" />
             <circle cx="80" cy="20" r="14" fill="black" />
@@ -37,16 +29,12 @@ export default function Header() {
             <line x1="84" y1="80" x2="58" y2="50" />
           </svg>
         </div>
+        <span className="mr-auto ml-[12px] text-[28px] font-extrabold">Spread Out</span>
         <button
           className={`opacity-0 pointer-events-none group-hover:pointer-events-auto transition-all duration-300 ease-in-out ${!leftAsideVisible && 'group-hover:opacity-100'}`}
           onClick={toggleAside}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 80"
-            width="40"
-            height="40"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 80" width="40" height="40">
             <rect width="100" height="12" rx="8"></rect>
             <rect y="30" width="100" height="12" rx="8"></rect>
             <rect y="60" width="100" height="12" rx="8"></rect>
